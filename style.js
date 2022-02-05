d0a805a3790bbf6a1656c25acf9fce596ceda16e
creatorckim <calvinkim4@yahoo.com>
2022-02-05 14:36:23 -0500

    added style file for app.js

---
+++ style.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    navContainer: {
+        width: '100%',
+        height:'100%',
+        backgroundColor: '#000',
+        color: '#ffc45d',
+      },
+      nav: {
+        backgroundColor: '#000',
+        color: '#ffc45d',
+      },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      actionBarContainer: {
+        position: 'absolute',
+        bottom: 0,
+        padding: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      addButtonContainer: {
+        width: 60,
+        height: 60,
+        backgroundColor: '#c51032',
+        borderRadius: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      addButton: {
+        fontSize: 30,
+        color: '#000',
+      },
+});
+
+export default styles;
