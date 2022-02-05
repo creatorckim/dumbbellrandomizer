1f689335ef0867581b073cae60ccdcb001de0de2
creatorckim <calvinkim4@yahoo.com>
2022-02-05 14:37:18 -0500

    added navigation stack and create routine button

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { back, bicep, chest, shoulder, tricep, quadricep, hamstring, glute, calf, ab } from './exercises';
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import styles from './style';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { back, bicep, forearm, postdelt, trapezius, chest, shoulder, tricep, quadricep, hamstring, glute, calf, ab, oblique } from './exercises';
 
-export default function App() {
-  return (
+
+function RoutineScreen() {
+  return(
     <View style={styles.container}>
-      <Text>Dumbbell Randomizer</Text>
       <StatusBar style="auto" />
+      <View style={styles.actionBarContainer}>
+          {/* <TouchableOpacity onPress={() => {navigation.navigate('NewRoutine')}}> */}
+          <TouchableOpacity>
+            <View style={styles.addButtonContainer}>
+              <Text style={styles.addButton}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+
+  return (
+    <NavigationContainer style={styles.navContainer}>
+      <Stack.Navigator initialRouteName='Routine'>
+        <Stack.Screen name='Routine' component={RoutineScreen} style={styles.nav} options={{ headerShown: false }}/>
+        {/* <Stack.Screen name='NewRoutine' component={NewRoutineScreen} style={styles.nav} options={{ headerShown: false }}/> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
